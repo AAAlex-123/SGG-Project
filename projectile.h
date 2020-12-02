322c2abb1971ad5e5f7861270c43fce8ee58e561
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-02 22:41:58 +0200

    U projectile.h: included set_path, update, draw
    also moved .cpp to .h

--- projectile.h
+++ projectile.h
@@ -3,11 +3,22 @@
 #include "Path.h"
 
 //Superclass for all projectiles
-class Projectile : public GameObject{
+class Projectile : public GameObject {
 public:
-	Projectile(float xpos, float ypos, float size, std::string * sprites,int vel,int damage) :
-		GameObject(xpos, ypos, size, sprites, 1, vel, damage, 1)
-	{};
-	virtual void update() override;
+	Projectile(float xpos, float ypos, float radius, std::string* sprites, int vel, int damage) :
+		GameObject(xpos, ypos, radius, sprites, 1, vel, damage, 1)
+	{ 
+		set_path(new StraightPath);
+	}
 
+	virtual void update() override
+	{
+		// movement->move(this->x, this->y, this->vel);
+		// if collides(GameObject&), do something
+	}
+
+	virtual void draw() const override
+	{
+		;
+	}
 };
