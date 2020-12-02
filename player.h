5ef86aa905505e97a5e6d35028a20810a3787df0
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-02 22:40:37 +0200

    U player.h: is now subclass of Entity
    also moved .cpp to .h

--- player.h
+++ player.h
@@ -1,13 +1,13 @@
 #pragma once
-#include "gameobject.h"
-#include "shooting.h"
-#include "projectile.h"
+#include "entity.h"
 
-class Player : public GameObject, public Shooting{
-private:
-
+class Player : public Entity {
 public:
-	virtual void update() override;
-	virtual void hit(GameObject& o2) override; //maybe play a sound idk lol
-
+	Player(float xpos, float ypos, float radius, std::string* sprites,
+		int sprite_no, int vel, int damage, int health) :
+		Entity(xpos, ypos, radius, sprites, sprite_no, vel, damage, health)
+	{
+		// set_projectile(new Projectile);
+		set_path(new PlayerPath);
+	}
 };
