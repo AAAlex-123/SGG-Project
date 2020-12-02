b4cb54ca5d44d905de986b58afbaa1933101a2d2
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-02 22:29:28 +0200

    U constants.h: too many constants

--- constants.h
+++ constants.h
@@ -3,6 +3,7 @@
 
 // global variables
 graphics::Brush br;
+
 // constants
 const short WINDOW_WIDTH = 1200;
 const short WINDOW_HEIGHT = 600;
@@ -23,13 +24,14 @@ enum game_states
 
 // function declarations
 void initialize();
+// ...
 
 void setColor(graphics::Brush&, const float*);
 void setColor(graphics::Brush&, char c);
 
 // assets
 const std::string asset_path = "assets\\";
-const std::string font = asset_path + "orange juice 2.0.ttf";
+const std::string font = asset_path + "orange juice 2.0.ttf"; // pog
 
 // key short names
 const graphics::scancode_t K_UP = graphics::scancode_t::SCANCODE_UP;
@@ -40,12 +42,3 @@ const graphics::scancode_t K_W = graphics::scancode_t::SCANCODE_W;
 const graphics::scancode_t K_A = graphics::scancode_t::SCANCODE_A;
 const graphics::scancode_t K_S = graphics::scancode_t::SCANCODE_S;
 const graphics::scancode_t K_D = graphics::scancode_t::SCANCODE_D;
-const graphics::scancode_t K_T = graphics::scancode_t::SCANCODE_T;
-const graphics::scancode_t K_F = graphics::scancode_t::SCANCODE_F;
-const graphics::scancode_t K_G = graphics::scancode_t::SCANCODE_G;
-const graphics::scancode_t K_H = graphics::scancode_t::SCANCODE_H;
-const graphics::scancode_t K_I = graphics::scancode_t::SCANCODE_I;
-const graphics::scancode_t K_J = graphics::scancode_t::SCANCODE_J;
-const graphics::scancode_t K_K = graphics::scancode_t::SCANCODE_K;
-const graphics::scancode_t K_L = graphics::scancode_t::SCANCODE_L;
-
