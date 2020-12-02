945fb23c5d6a3102269623f0dcade8da2b967ec4
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-02 22:43:20 +0200

    U visual_effect.h: now has a constructor
    also moved everything from .cpp to .h

--- visual_effect.h
+++ visual_effect.h
@@ -3,6 +3,18 @@
 
 class VisualEffect : public Drawing
 {
+private:
+	float duration, fps;
+
 public:
-	virtual void draw() const override;
+	VisualEffect(float xpos, float ypos, float radius, std::string* sprites, int sprite_no,
+		float duration, float fps) :
+		Drawing(xpos, ypos, radius, sprites, sprite_no),
+		duration(duration), fps(fps)
+	{ ; }
+
+	virtual void draw() const override
+	{
+		;
+	}
 };
