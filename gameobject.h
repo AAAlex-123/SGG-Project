c01541f801d4c6f7c626478cb6d20a7f2eeb293c
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-02 22:39:19 +0200

    U gameobject.h: optimizing
    optimized two methods
    added methods to set path

--- gameobject.h
+++ gameobject.h
@@ -2,37 +2,40 @@
 #include "drawing.h"
 #include "Path.h"
 
-
-//Superclass for all interactive objects in the game
-class GameObject : public Drawing{
+// Superclass for all interactive objects in the game
+class GameObject : public Drawing {
 protected:
 	int vel, damage, health;
-	bool isDead = false;
+	bool isDead;
+
 	static Path* movement;
 
 public:
+	GameObject(float xpos, float ypos, float radius, std::string* sprites,
+				int sprite_no, int vel, int damage, int health) :
+		Drawing(xpos, ypos, radius, sprites, sprite_no),
+		vel(vel), damage(damage), health(health), isDead(false)
+	{ ; }
 
-	GameObject(float xpos, float ypos, float size, std::string* sprites, int frames, int vel, int damage, int health) :
-		Drawing(xpos, ypos, size, sprites, frames),
-		vel(vel),
-		damage(damage),
-		health(health)
-	{};
-
-	//Routine called by the update() main function
+	// Routines called by the update(), draw() main functions
 	virtual void update() = 0;
+	virtual void draw() const = 0;
 
-	//Behavior when hit by another object
-	virtual void hit(GameObject& o2) { //does this need to be virtual? 
+	// sets the class' path, normally will be called once
+	// in the constructor of the first object of a class
+	///// MAYBE VIRTUAL SO SUBCLASSES SET THEIR OWN STATIC MEMBER? /////
+	void set_path(Path* p) { this->movement = p; }
+
+	// Behavior when hit by another object
+	void hit(GameObject& o2) {
 		this->health =- o2.damage;
-		if (health <= 0)  isDead = true;
+		isDead = health <= 0;
 	}
 
-	//Calculates whether this has collided with another hitbox
-	bool wasHit(GameObject& o2) const {
-		float d = sqrt(pow((o2.x - this->x), 2) + pow((o2.y - this->y), 2)); //overflow will be fine...probably?
-		return (d - this->size - o2.size) < 0;
+	// Calculates whether this has collided with another hitbox
+	// yes this is more optimized, compiler might do this by itself though
+	bool collides(GameObject& o2) const {
+		return ((((o2.x - this->x) * (o2.x - this->x)) + ((o2.y - this->y) * (o2.y - this->y)))
+			< (this->radius - o2.radius) * (this->radius - o2.radius));
 	}
-
-	virtual void draw() const override;
 };
