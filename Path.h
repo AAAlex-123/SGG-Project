5ceaf7685e7e347bca39d992bb3fcb59548a5ef3
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-02 22:27:00 +0200

    U path.h: added PlayerPath, bugfix

--- Path.h
+++ Path.h
@@ -1,20 +1,27 @@
 #pragma once
-//name is capitalized due to VS reserving the lowercase name
 
 //Interface for all Path classes
-//Path is a Strategy changing its parent class x and y coordinates
+//Path is a Strategy changing its parent class' x and y coordinates
 class Path {
 public:
-	virtual void move(float & x,float & y, const int vel) = 0;
+	virtual void move(float& x, float& y, const int vel) = 0;
 };
 
 class StraightPath: public Path {
 public:
 	virtual void move(float& x, float& y, const int vel) override {
-		x += vel; y += vel;
+		y += vel;
 	}
 };
 
 class SwigglyPath : public Path {
-//...
+	//...
+};
+
+// special case for player
+class PlayerPath : public Path {
+public:
+	virtual void move(float& x, float& y, const int vel) override {
+		;
+	}
 };
