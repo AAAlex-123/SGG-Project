6251113852f83330fb5bbd16663deec423bc091d
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-03 20:58:05 +0200

    U gameobject.h: moved declarations to .cpp
    movement is no longer static because constructor initializes it
    removed set_path because it shouldn't exist (?)

--- gameobject.h
+++ gameobject.h
@@ -8,34 +8,19 @@ protected:
 	int vel, damage, health;
 	bool isDead;
 
-	static Path* movement;
+	Path* movement;
 
 public:
 	GameObject(float xpos, float ypos, float radius, std::string* sprites,
-				int sprite_no, int vel, int damage, int health) :
-		Drawing(xpos, ypos, radius, sprites, sprite_no),
-		vel(vel), damage(damage), health(health), isDead(false)
-	{ ; }
+		int sprite_no, int vel, int damage, int health, Path*);
 
 	// Routines called by the update(), draw() main functions
 	virtual void update() = 0;
 	virtual void draw() const = 0;
 
-	// sets the class' path, normally will be called once
-	// in the constructor of the first object of a class
-	///// MAYBE VIRTUAL SO SUBCLASSES SET THEIR OWN STATIC MEMBER? /////
-	void set_path(Path* p) { this->movement = p; }
-
 	// Behavior when hit by another object
-	void hit(GameObject& o2) {
-		this->health =- o2.damage;
-		isDead = health <= 0;
-	}
+	void hit(GameObject& o2);
 
-	// Calculates whether this has collided with another hitbox
-	// yes this is more optimized, compiler might do this by itself though
-	bool collides(GameObject& o2) const {
-		return ((((o2.x - this->x) * (o2.x - this->x)) + ((o2.y - this->y) * (o2.y - this->y)))
-			< (this->radius - o2.radius) * (this->radius - o2.radius));
-	}
+	// Calculates whether this object collides with another hitbox
+	bool collides(GameObject& o2) const;
 };
