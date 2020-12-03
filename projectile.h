81ebe75c510fc0bb15d2c8f7b5bfbd8574288a1b
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-03 20:56:56 +0200

    U projectile.h: moved declarations to .cpp
    Path is now given at the constructor because of factory method

--- projectile.h
+++ projectile.h
@@ -2,23 +2,11 @@
 #include "gameobject.h"
 #include "Path.h"
 
-//Superclass for all projectiles
 class Projectile : public GameObject {
 public:
-	Projectile(float xpos, float ypos, float radius, std::string* sprites, int vel, int damage) :
-		GameObject(xpos, ypos, radius, sprites, 1, vel, damage, 1)
-	{ 
-		set_path(new StraightPath);
-	}
+	Projectile(float xpos, float ypos, float radius, std::string* sprites, int vel, int damage, Path*);
 
-	virtual void update() override
-	{
-		// movement->move(this->x, this->y, this->vel);
-		// if collides(GameObject&), do something
-	}
+	virtual void update() override;
 
-	virtual void draw() const override
-	{
-		;
-	}
+	virtual void draw() const override;
 };
