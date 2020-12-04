0979fcedfb92223dce7df9cdd444f28a1a47398f
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-04 15:06:47 +0200

    A entity_factory: a factory to create entities

---
+++ entity_factory.cpp
@@ -0,0 +1,20 @@
+#include "entity_factory.h"
+
+
+Entity* EntityFactory::createPlayer(float xpos, float ypos, float radius, std::string* sprites,
+	int sprite_no, int vel, int damage, int health, Path*, Projectile*)
+{
+	;
+}
+
+Entity* EntityFactory::createEnemy1(float xpos, float ypos, float radius, std::string* sprites,
+	int sprite_no, int vel, int damage, int health, Path*, Projectile*)
+{
+	;
+}
+
+Entity* EntityFactory::createEnemy2(float xpos, float ypos, float radius, std::string* sprites,
+	int sprite_no, int vel, int damage, int health, Path*, Projectile*)
+{
+	;
+}
