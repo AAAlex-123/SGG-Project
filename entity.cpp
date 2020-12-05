c7b4607266754152d9dc6e123f012f9e00e01499
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-12-05 16:25:23 +0200

    Update entity.cpp

--- entity.cpp
+++ entity.cpp
@@ -1,19 +1,24 @@
 #include "entity.h"
 
-Entity::Entity(float xpos, float ypos, float angle, float radius, std::string* sprites,
-	int sprite_no, int vel, int damage, int health, Path* path, Projectile* proj) :
-	GameObject(xpos, ypos, angle, radius, sprites, sprite_no, vel, damage, health, path),
-	curr_projectile(proj)
+Entity::Entity(float xpos, float ypos, float angle, float radius,std::string* sprites, int vel, int damage, int health, Path* path, int proj_type) :
+	GameObject(xpos, ypos,angle, radius, sprites, vel, damage, health, path),
+	curr_projectile(proj_type)
+{}
+
+void Entity::update()
 {
-	;
+	//do stuff lol
 }
 
-void Entity::update(float ms)
-{
-	;
+void Entity::setProjectile(int proj_type) {
+	this->curr_projectile = proj_type;
 }
 
-void Entity::fire() const
+Projectile& Entity::fire() const
 {
-	;
+	//return Factory.create(this.proj_type)
+}
+
+Entity::~Entity() {
+	delete& curr_projectile;
 }
