a29fc4da5fbffdbaa34a3101d083d6ffa1b60bb8
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-05 11:46:12 +0200

    U drawing.h: added draw
    draw() method can now be implemented at this level with the help of the angle and curr_sprite member variables.
    objects that animate simply change the curr_sprite variable to display a different sprite.
    Drawing is no longer abstract though xd

--- drawing.h
+++ drawing.h
@@ -1,23 +1,33 @@
 #pragma once
+#include "graphics.h"
 #include <string>
 
 //Interface to allow uniform access to all drawable objects
 class Drawing {
 
+private:
+graphics::Brush br;
+
 public:
 	std::string* sprites;
 
-	float x, y, radius;
+	float x, y, angle, radius;
 
 	//amount of sprite strings in the array
-	int sprite_no;
+	int sprite_no, curr_sprite;
 
-	Drawing(float xpos, float ypos, float radius, std::string* sprites, int sprite_no) :
-		x(xpos), y(ypos),
+	virtual void draw()
+	{
+		br.texture = sprites[curr_sprite];
+		graphics::setOrientation(angle);
+		graphics::drawRect(x, y, 2 * radius, 2 * radius, br);
+	}
+
+	Drawing(float xpos, float ypos, float angle, float radius, std::string* sprites, int sprite_no) :
+		br(),
 		sprites(sprites),
-		radius(radius),
-		sprite_no(sprite_no)
-	{};
-
-	virtual void draw() const = 0;
+		x(xpos), y(ypos), angle(angle), radius(radius),
+		sprite_no(sprite_no),
+		curr_sprite(0)
+	{ ; }
 };
