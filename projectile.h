7d93b39d49328246ebcb0325ae147fb635b429bb
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-12-05 18:59:10 +0200

    Update entity.h (#6)
    * Update entity.h
    
    Η ΒΙΟΜΗΧΑΝΙΚΗ ΕΠΑΝΑΣΤΑΣΗ ΚΑΙ ΟΙ ΕΠΙΠΤΩΣΕΙΣ ΤΗΣ
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete entity_factory.cpp
    
    * Delete entity_factory.h
    
    * Update projectile.h

--- projectile.h
+++ projectile.h
@@ -8,9 +8,9 @@ public:
 	const static int STANDARD = 1;
 	//...
 
-
-	Projectile(float xpos, float ypos, float angle, float radius, std::string* sprites, int vel, int damage, Path*);
-
 	virtual void update(float ms) override;
 
+private:
+	Projectile(float xpos, float ypos, float angle, float radius, std::string* sprites, int vel, int damage, Path* p);
+	friend class GObjFactory; //allow creation of projectiles only to the factory
 };
