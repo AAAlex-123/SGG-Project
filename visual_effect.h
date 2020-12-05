9b59a2fc10926a46d9695a85686e016e605e0287
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-05 16:18:21 +0200

    U visual_effect.h: now works
    added operator bool() for a better way to indicate when an object should be destroyed
    update(ms) changes the sprites according to how much time has passed

--- visual_effect.h
+++ visual_effect.h
@@ -4,17 +4,28 @@
 class VisualEffect : public Drawing
 {
 private:
-	float duration, fps;
+	float duration, spf, elapsed, total_elapsed;
 
 public:
-	VisualEffect(float xpos, float ypos, float radius, std::string* sprites, int sprite_no,
+	VisualEffect(float xpos, float ypos, float angle, float radius, std::string* sprites, int sprite_no,
 		float duration, float fps) :
-		Drawing(xpos, ypos, radius, sprites, sprite_no),
-		duration(duration), fps(fps)
+		Drawing(xpos, ypos, angle, radius, sprites, sprite_no),
+		duration(duration), spf(1/fps), elapsed(0), total_elapsed(0)
 	{ ; }
 
-	virtual void draw() const override
+	// true if the object should be destroyed
+	operator bool() const
 	{
-		;
+		return total_elapsed > duration;
+	}
+
+	// changes the current sprite according to elapsed time
+	void update(float ms)
+	{
+		total_elapsed += ms / 1000.0f;
+		elapsed += ms / 1000.0f;
+
+		curr_sprite = (((curr_sprite + 1) % sprite_no) * (elapsed > spf)) + (curr_sprite * !(elapsed > spf));
+		elapsed = (0.0f * (elapsed > spf)) + (elapsed * !(elapsed > spf));
 	}
 };
