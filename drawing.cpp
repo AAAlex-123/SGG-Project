d52c10b4176db71b62f3ba7d163ee3e4fe0962e0
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-05 17:02:29 +0200

    U drawing: hotfixed
    removed const because br.texture is changed

--- drawing.cpp
+++ drawing.cpp
@@ -1,15 +1,14 @@
 #include "drawing.h"
 
-graphics::Brush Drawing::br;
-
 Drawing::Drawing(float xpos, float ypos, float angle, float radius, const std::string* sprites) :
 	x(xpos), y(ypos),
 	angle(angle),
 	sprites(sprites),
-	radius(radius)
-{};
+	radius(radius),
+	curr_sprite(0)
+{}
 
-void Drawing::draw() const{
+void Drawing::draw() {
 	br.texture = *(setNewFrame(50));//idk what value you want to put here
 	graphics::setOrientation(angle);
 	graphics::drawRect(this->x, this->y, this->radius * 2, this->radius * 2, br);
