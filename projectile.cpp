dd8f61f500bdb37ea62697e7ff74af638b599f0c
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-12-06 13:38:39 +0200

    Update projectile.cpp
    removed herobrine

--- projectile.cpp
+++ projectile.cpp
@@ -4,9 +4,3 @@ Projectile::Projectile(float xpos, float ypos, float angle, float radius, std::s
 	GameObject(xpos, ypos, angle, radius, sprites, vel, damage, 1, p)
   {}
 
-void Projectile::update(float ms) 
-{
-	;
-	// movement->move(this->x, this->y, this->vel);
-	// if collides(GameObject&), do something
-}
