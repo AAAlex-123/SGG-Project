24ebee83b964477062d6753b04e142d8b4b9b00b
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-12-06 13:40:03 +0200

    Update projectile.h

--- projectile.h
+++ projectile.h
@@ -2,14 +2,8 @@
 #include "gameobject.h"
 #include "Path.h"
 
+//Interface for all projectile classes. Different objects are built via composition.
 class Projectile : public GameObject {
-public:
-
-	const static int STANDARD = 1;
-	//...
-
-	virtual void update(float ms) override;
-
 private:
 	Projectile(float xpos, float ypos, float angle, float radius, std::string* sprites, int vel, int damage, Path* p);
 	friend class GObjFactory; //allow creation of projectiles only to the factory
