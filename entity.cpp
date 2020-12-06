35cd50416574edef7724ce227609f468f19c1ada
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-12-06 13:33:48 +0200

    Update entity.cpp
    added fire()

--- entity.cpp
+++ entity.cpp
@@ -1,25 +1,20 @@
 #include "entity.h"
+#include "GObjFactory.h"
 
 Entity::Entity(float xpos, float ypos, float angle, float radius,std::string* sprites, int vel, int damage, int health, Path* path, int proj_type) :
 	GameObject(xpos, ypos,angle, radius, sprites, vel, damage, health, path),
 	curr_projectile(proj_type)
 {}
 
-void Entity::update(float ms)
-{
-	//do stuff lol
-}
-
 void Entity::setProjectile(int proj_type) {
 	this->curr_projectile = proj_type;
 }
 
-/*
-Projectile& Entity::fire() const
-{
-	//return Factory.create(this.proj_type)
+
+Projectile& Entity::fire() const{
+	return GObjFactory::createProjectile(this->curr_projectile,this->y+this->radius+2,this->y,this->angle);
 }
-*/
+
 
 Entity::~Entity() {
 	delete& curr_projectile;
