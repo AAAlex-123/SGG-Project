69a98a933b2ae32d4807b6d47341a187c65e480d
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-06 15:16:04 +0200

    U gameobject.h: same as gameobject.cpp

--- gameobject.h
+++ gameobject.h
@@ -1,19 +1,16 @@
 #pragma once
 #include "drawing.h"
-#include "Path.h"
 
 // Superclass for all interactive objects in the game
 class GameObject : public Drawing {
 
 protected:
-	const int vel, damage;
+	const int damage;
 	int health;
 	bool isDead; //replace with bool op
 
-	Path* movement;
-
 public:
-	GameObject(float xpos, float ypos, float angle, float radius, const std::string* sprites, int vel, int damage, int health, Path*);
+	GameObject(float xpos, float ypos, float angle, float vel, float radius, const std::string* sprites, Path*, int damage, int health);
 
 	// Routines called by the update(), draw() main functions
 	virtual void update(float ms) = 0;
