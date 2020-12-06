9a7c3972f6cd3e03c16669da5e9c7563d355b297
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-06 15:17:59 +0200

    U drawing: updated constructor and draw methods

--- drawing.cpp
+++ drawing.cpp
@@ -1,15 +1,16 @@
 #include "drawing.h"
 
-Drawing::Drawing(float xpos, float ypos, float angle, float radius, const std::string* sprites) :
-	x(xpos), y(ypos),
-	angle(angle),
+Drawing::Drawing(float xpos, float ypos, float angle, float vel, float radius, const std::string* sprites, Path* movement) :
 	sprites(sprites),
+	x(xpos), y(ypos), angle(angle), vel(vel),
 	radius(radius),
-	curr_sprite(0)
+	curr_sprite(0),
+	movement(movement)
 {}
 
-void Drawing::draw() {
-	br.texture = *(setNewFrame(50));//idk what value you want to put here
-	graphics::setOrientation(angle);
+void Drawing::draw()
+{
+	br.texture = sprites[curr_sprite];
+	graphics::setOrientation(angle / (2 * PI) * 360);
 	graphics::drawRect(this->x, this->y, this->radius * 2, this->radius * 2, br);
 }
