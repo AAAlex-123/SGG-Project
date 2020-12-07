43f08f210f426d53110f477be2082d20e922685a
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-07 18:26:59 +0200

    U drawing.h: added op bool() and stuff
    destructor, draw() is virtual and final, comments, proper {

--- drawing.h
+++ drawing.h
@@ -5,15 +5,15 @@
 #include <string>
 
 //Interface to allow uniform access to all drawable objects. Includes a standard drawing method all classes need to use.
-class Drawing {
-
+class Drawing
+{
 private:
 	graphics::Brush br;
 
 protected:
 	const std::string* const sprites;
 
-	float x, y, angle, vel;
+	float x, y, angle, vel;	// angle is measured in radiants and not in degrees
 	const float radius;
 	int curr_sprite;
 
@@ -22,7 +22,11 @@ protected:
 public:
 	Drawing(float xpos, float ypos, float angle, float vel, float radius, const std::string* sprites, Path*);
 
-	void draw();
+	// Draws the object at [x, y] with `angle` orientation with its `curr_sprite`
+	virtual void draw() final;
 
-	virtual ~Drawing() {}
+	// Returns false if the object is not alive; i.e. it must be destroyed
+	virtual operator bool() const = 0;
+
+	virtual ~Drawing();
 };
