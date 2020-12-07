a04620eb65566be058e83d7adf6894cb3d424cb3
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-07 18:29:09 +0200

    U gameobject: added op bool(), adjusted everything

--- gameobject.cpp
+++ gameobject.cpp
@@ -1,31 +1,32 @@
 #include "gameobject.h"
-#include "constants.h"
 
 GameObject::GameObject(float xpos, float ypos, float angle, float vel, float radius, const std::string* sprites, Path* p, int damage, int health) :
 	Drawing(xpos, ypos, angle, vel, radius, sprites, p),
-	damage(damage), health(health), isDead(false)
+	damage(damage), health(health)
 {}
 
-void GameObject::hit(GameObject& o2){
-	this->health = -o2.damage;
-	isDead = health <= 0;
+void GameObject::hit(GameObject& o2)
+{
+	this->health -= o2.damage;
 }
 
-bool GameObject::collides(GameObject& o2) const{
+bool GameObject::collides(GameObject& o2) const
+{
 	return ((((o2.x - this->x) * (o2.x - this->x)) + ((o2.y - this->y) * (o2.y - this->y)))
 		< (this->radius - o2.radius) * (this->radius - o2.radius));
 }
 
-void GameObject::update(float ms){
+void GameObject::update(float ms)
+{
 	this->movement->move(this->x, this->y, this->angle, this->vel, ms);
-	if (this->x > CANVAS_WIDTH || this->x<0 || this->y >CANVAS_HEIGHT || this->y < 0)
-		isDead = true;
 }
 
-GameObject::operator bool() const{
-	return !this->isDead;
+GameObject::operator bool() const
+{
+	return (this->x < get_canvas_width() && this->x > 0 && this->y < get_canvas_height() && this->y > 0) && (health > 0);
 }
 
-GameObject::~GameObject(){
-	delete movement,sprites;
+GameObject::~GameObject()
+{
+	delete movement, sprites;
 }
