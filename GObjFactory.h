12de27446099b13f6ee858c625e0c2d7a7a10242
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-07 10:19:59 +0200

    U GObjFactory.h: same as GObjFactory.cpp

--- GObjFactory.h
+++ GObjFactory.h
@@ -1,29 +1,26 @@
 #pragma once
 #include "entity.h"
 #include "projectile.h"
+#include "globals.h"
 
-#include "scancodes.h"
-typedef graphics::scancode_t key;
-
-class GObjFactory{
+class GObjFactory {
 private:
 	const static int std_size = 30;
 	const static int std_speed = 35;
 	static Entity& createEnemy1(float x, float y, float angle);
 	static Entity& createEnemy2(float x, float y, float angle);
 	static Entity& createEnemy3(float x, float y, float angle);
-	static Entity& createPlayer(float x, float y, float angle, float dangle, key u, key d, key l, key r, key rl, key rr);
+	static Entity& createPlayer(float x, float y, float angle, float dangle, Keyset keyset);
 
 	static Projectile& createStdB(float x, float y, float angle);
 	static Projectile& createLB(float x, float y, float angle);
 	static Projectile& createIncB(float x, float y, float angle);
-	
-public:
-	const static int STANDARD_BULLET = 10, LIGHT_BULLET = 11, INCEND_BULLET = 12; 
-	const static int PLAYER = 0, ENEMY_1 = 1, ENEMY_2 = 2, ENEMY_3 =3;
-	
-	static Entity& createEntity(int type, float x, float y, float angle,
-		float dangle = 0.0f, key u = key::SCANCODE_Z, key d = key::SCANCODE_Z, key l = key::SCANCODE_Z, key r = key::SCANCODE_Z, key rl = key::SCANCODE_Z, key rr = key::SCANCODE_Z);
-	static Projectile& createProjectile(int type, float x, float y,float angle);
 
+public:
+	const static int STANDARD_BULLET = 10, LIGHT_BULLET = 11, INCEND_BULLET = 12;
+	const static int PLAYER = 0, ENEMY_1 = 1, ENEMY_2 = 2, ENEMY_3 = 3;
+
+	static Entity& createEntity(int type, float x, float y, float angle, float dangle = 0.0f,
+			Keyset keyset = Keyset(key::SCANCODE_Z, key::SCANCODE_Z, key::SCANCODE_Z, key::SCANCODE_Z, key::SCANCODE_Z, key::SCANCODE_Z));
+	static Projectile& createProjectile(int type, float x, float y, float angle);
 };
