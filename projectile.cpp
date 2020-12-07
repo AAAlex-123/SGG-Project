396c828406b9bba1e76be4297b8ebc81f7529901
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-12-07 13:38:53 +0200

    Update projectile.cpp

--- projectile.cpp
+++ projectile.cpp
@@ -3,7 +3,9 @@
 Projectile::Projectile(float xpos, float ypos, float angle, float vel, float radius, std::string* sprites, Path* p, int damage) :
 	GameObject(xpos, ypos, angle, vel, radius, sprites, p, damage, 1)
 {}
-
+Projectile::~Projectile(){
+	delete p;
+}
 void Projectile::update(float ms)
 {
 	this->movement->move(this->x, this->y, this->angle, this->vel, ms);
