ca0117ad66534b71730bd4fbbb2567e8cee2a51b
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-07 18:32:11 +0200

    U projectile.h: update is now public

--- projectile.h
+++ projectile.h
@@ -1,16 +1,16 @@
 #pragma once
 #include "gameobject.h"
-#include "Path.h"
 
 //Interface for all projectile classes. Different objects are built via composition.
-class Projectile : public GameObject {
-
+class Projectile : public GameObject
+{
 private:
-
 	Projectile(float xpos, float ypos, float angle, float vel, float radius, std::string* sprites, Path*, int damage);
-
-	virtual void update(float ms) override;
 	friend class GObjFactory; //allow creation of projectiles only to the factory
+
 public:
+	// Routine called by the update() main function; updated the object's state
+	virtual void update(float ms) override;
+	
 	virtual ~Projectile();
 };
