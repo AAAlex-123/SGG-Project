eea5c771913a44e5248d7805a222cb45c565cf16
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-12-07 19:48:30 +0200

    Update gameobject.cpp

--- gameobject.cpp
+++ gameobject.cpp
@@ -5,28 +5,23 @@ GameObject::GameObject(float xpos, float ypos, float angle, float vel, float rad
 	damage(damage), health(health)
 {}
 
-void GameObject::hit(GameObject& o2)
-{
+void GameObject::hit(GameObject& o2){
 	this->health -= o2.damage;
 }
 
-bool GameObject::collides(GameObject& o2) const
-{
+bool GameObject::collides(GameObject& o2) const{
 	return ((((o2.x - this->x) * (o2.x - this->x)) + ((o2.y - this->y) * (o2.y - this->y)))
 		< (this->radius - o2.radius) * (this->radius - o2.radius));
 }
 
-void GameObject::update(float ms)
-{
+void GameObject::update(float ms){
 	this->movement->move(this->x, this->y, this->angle, this->vel, ms);
 }
 
-GameObject::operator bool() const
-{
+GameObject::operator bool() const{
 	return (this->x < get_canvas_width() && this->x > 0 && this->y < get_canvas_height() && this->y > 0) && (health > 0);
 }
 
-GameObject::~GameObject()
-{
+GameObject::~GameObject(){
 	delete movement, sprites;
 }
