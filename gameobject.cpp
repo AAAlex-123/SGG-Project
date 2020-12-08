737f6b50a4bc79d50e098fddcd8f1ac44016f562
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-08 16:53:06 +0200

    U gameobject: added const, fixed destructor
    Drawing::~Drawing is in charge of deleting movement and sprites and at this moment both were destroying them lmao

--- gameobject.cpp
+++ gameobject.cpp
@@ -5,23 +5,21 @@ GameObject::GameObject(float xpos, float ypos, float angle, float vel, float rad
 	damage(damage), health(health)
 {}
 
-void GameObject::hit(GameObject& o2){
+void GameObject::hit(const GameObject& o2) {
 	this->health -= o2.damage;
 }
 
-bool GameObject::collides(GameObject& o2) const{
+bool GameObject::collides(const GameObject& o2) const {
 	return ((((o2.x - this->x) * (o2.x - this->x)) + ((o2.y - this->y) * (o2.y - this->y)))
 		< (this->radius - o2.radius) * (this->radius - o2.radius));
 }
 
-void GameObject::update(float ms){
+void GameObject::update(float ms) {
 	this->movement->move(this->x, this->y, this->angle, this->vel, ms);
 }
 
-GameObject::operator bool() const{
-	return (this->x < get_canvas_width() && this->x > 0 && this->y < get_canvas_height() && this->y > 0) && (health > 0);
+GameObject::operator bool() const {
+	return (this->x < get_canvas_width() && this->x > 0 && this->y < get_canvas_height() && this->y > 0) && (this->health > 0);
 }
 
-GameObject::~GameObject(){
-	delete movement, sprites;
-}
+GameObject::~GameObject() {}
