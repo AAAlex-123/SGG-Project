2f3cad8cc2c0ebd040dff227ab95c2dcda7bfabf
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-10 16:15:52 +0200

    U level.h: added waves

--- level.h
+++ level.h
@@ -5,12 +5,42 @@
 class Level
 {
 private:
+	class Wave;
 	int _id;
 	std::string _desc;
 
-	std::queue<std::pair<float, Entity*>> enemies;
+	std::queue<std::pair<float, Wave*>> waves;
 
 	float _total_time;
+
+	class Wave
+	{
+	private:
+		int _id;
+		std::string _desc;
+
+		std::queue<std::pair<float, Entity*>> enemies;
+
+		float _total_time;
+
+	public:
+		void update(float ms) { _total_time += (ms / 1000.0f); }
+		bool can_spawn()
+		{
+			if (enemies.empty())
+				return false;
+			return _total_time > enemies.front().first;
+		}
+
+		Entity& spawn()
+		{
+			Entity* return_value = enemies.front().second;
+			enemies.pop();
+			return *(return_value);
+		}
+
+		void add_enemy(float time, Entity* enemy) { enemies.push(std::pair<float, Entity*>(time, enemy)); }
+	};
 
 public:
 	// default constructor needed for some reason
@@ -18,20 +48,25 @@ public:
 
 	void operator=(const Level& rhs);
 
-	inline void update(float ms) { _total_time += (ms / 1000.0f); }
-	inline bool can_spawn()
+	void update(float ms) { _total_time += (ms / 1000.0f); waves.front().second->update(ms); }
+	bool can_spawn()
 	{
-		if (enemies.empty())
+		if (waves.empty())
 			return false;
-		return _total_time > enemies.front().first;
+		return waves.front().second->can_spawn();
 	}
 
-	Entity& spawn();
+	Entity& spawn()
+	{
+		Entity* return_value = &waves.front().second->spawn();
+		waves.pop();
+		return *(return_value);
+	}
 
-	inline void add_enemy(float time, Entity* enemy) { enemies.push(std::pair<float, Entity*>(time, enemy)); }
+	void add_enemy(float time, Entity* enemy) { waves.back().second->add_enemy(time, enemy); }
 
-	inline int id() { return _id; }
-	inline std::string info() { return "level " + std::to_string(id()) + ": " + _desc; }
+	int id() { return _id; }
+	std::string info() { return "level " + std::to_string(id()) + ": " + _desc; }
 	
 	// returns the string necessary to rebuild this level
 	std::string to_file_string();
