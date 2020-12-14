12aa9b09afde735c96bfd8e5fcee5a16a72d04c9
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-12-14 13:42:11 +0200

    Update game_data.h
    89

--- game_data.h
+++ game_data.h
@@ -15,9 +15,12 @@ struct GameData {
 private:
 	bool load_levels_from_file(const std::string& levels_path);
 
+	template<class T>
+	void deleteList(list<T*>* ls);
+
 public:
 	list<Entity*>* enemyLs = new list<Entity*>;
-	list<Entity*>* playerLs = new list<Entity*>; //uniform access to either 1 or 2 players or more if we do socket stuff (:
+	list<Entity*>* playerLs = new list<Entity*>; //uniform access to either 1 or 2 players and definitely no more
 	list<Projectile*>* enemyProjLs = new list<Projectile*>;
 	list<Projectile*>* playerProjLs = new list<Projectile*>;
 	list<VisualEffect*>* effectsLs = new list<VisualEffect*>;
@@ -41,24 +44,87 @@ public:
 	GameData();
 	
 	//Updates all objects within the list. Template class must be derived from Drawing.
-	template <class T>
-	void update(float ms, list<T*>*);
-	
+	template<class T>
+	void update(float ms, list<T*>* ls);
+
 	// Draws all objects within the list. Template class must be derived from Drawing.
 	template <class T>
-	void draw(list<T*>* ls);
+	void draw(list<T*>* ls) const;
 
 	//Checks collisions between 2 lists. Template classes must both be derived from GameObject.
-	template <class T1,class T2>
-	void checkCollisions(list<T1*>*, list<T2*>*);
+	template<class T1, class T2>
+	void checkCollisions(list<T1*>* ls1, list<T2*>* ls2);
 
 	//Spawns a projectile for every eligible object in the list. Template class must be derived from Entity.
-	template <class T>
-	void fire(list<T*>*) const;
+	template<class T>
+	void fire(list<T*>* ls) const;
 
 	//Checks if any object within the list must be destroyed, and deletes it. Template class must be derived from Drawing.
-	template <class T>
-	void checkAndDelete(list<T*>*);
+	template<class T>
+	void checkAndDelete(list<T*>* ls);
+
+
 
 	~GameData();
 };
+
+template<class T>
+void GameData::update(float ms, list<T*>* ls) {
+	for (Drawing* dr : ls) {
+		dr->update(ms);
+	}
+}
+
+template <class T>
+void GameData::draw(list<T*>* ls) const {
+	for (Drawing* dr : ls) {
+		dr->draw();
+	}
+}
+
+template<class T>
+void GameData::fire(list<T*>* ls) const {
+	bool isPlayer = false;
+
+	for (Entity* en : ls) {
+
+		if (en->hasFired) {
+			//check if projectile was launched by a player
+			for (Entity* pl : playerLs)
+				isPlayer |= pl == en;	// fancy
+
+			if (isPlayer) {
+				playerProjLs->push_back(en->getProjectile());
+			}
+			else {
+				enemyProjLs->push_back(en->getProjectile());
+			}
+		}
+
+	}
+}
+template<class T1, class T2>
+void GameData::checkCollisions(list<T1*>* ls1, list<T2*>* ls2) {
+	for (GameObject* o1 : ls1)
+		for (GameObject* o2 : ls2)
+			o1->collides(o2);
+}
+
+template<class T>
+void GameData::checkAndDelete(list<T*>* ls) {
+	list<T*>::iterator iter;
+	for (iter = ls.begin(); iter != ls.end(); ++iter) {
+		if (!*iter) {
+			delete* iter;
+			iter = ls->erase(iter);
+		}
+	}
+}
+
+
+template<class T>
+void GameData::deleteList(list<T*>* ls) {
+	for (T obj : ls)
+		delete obj;
+	delete ls;
+}
