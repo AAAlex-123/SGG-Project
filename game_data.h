b1523fddbd61ad8de6fecd3fda5917d1dc3aec26
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-14 10:16:52 +0200

    U game_data.h: added some includes

--- game_data.h
+++ game_data.h
@@ -2,7 +2,11 @@
 #include "projectile.h"
 #include "entity.h"
 #include "visual_effect.h"
+#include "level.h"
+#include "constants.h"
 #include <list>
+#include <vector>
+#include <unordered_map>
 
 // lmao imagine using using
 using namespace std;
@@ -13,7 +17,7 @@ private:
 
 public:
 	list<Entity*>* enemyLs = new list<Entity*>;
-	list<Entity*>* playerLs = new list<Entity*>; //uniform access to either 1 or 2 players
+	list<Entity*>* playerLs = new list<Entity*>; //uniform access to either 1 or 2 players or more if we do socket stuff (:
 	list<Projectile*>* enemyProjLs = new list<Projectile*>;
 	list<Projectile*>* playerProjLs = new list<Projectile*>;
 	list<VisualEffect*>* effectsLs = new list<VisualEffect*>;
@@ -32,14 +36,12 @@ public:
 	// levels
 	std::unordered_map<int, Level> levels;
 	int curr_active_level, curr_selected_level;
-
-	// spawn will go in the levels
 	
 	// constructor because why not
 	GameData();
 	
 	//Updates all objects within the list. Template class must be derived from Drawing.
-	template<class T>
+	template <class T>
 	void update(float ms, list<T*>*);
 	
 	// Draws all objects within the list. Template class must be derived from Drawing.
@@ -47,15 +49,15 @@ public:
 	void draw(list<T*>* ls);
 
 	//Checks collisions between 2 lists. Template classes must both be derived from GameObject.
-	template<class T1,class T2>
+	template <class T1,class T2>
 	void checkCollisions(list<T1*>*, list<T2*>*);
 
 	//Spawns a projectile for every eligible object in the list. Template class must be derived from Entity.
-	template<class T>
+	template <class T>
 	void fire(list<T*>*) const;
 
 	//Checks if any object within the list must be destroyed, and deletes it. Template class must be derived from Drawing.
-	template<class T>
+	template <class T>
 	void checkAndDelete(list<T*>*);
 
 	~GameData();
