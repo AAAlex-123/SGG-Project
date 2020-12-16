22007b5dbf4dd89dde7b81375d64729210ce1d91
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-16 12:27:49 +0200

    U game_data: proper list initialization

--- game_data.cpp
+++ game_data.cpp
@@ -6,7 +6,7 @@
 GameData::GameData() : fps(0), game_state(0),
 	el(0.0f), sps(4.0f), curr_img(0), images(),
 	levels(), curr_active_level(-1), curr_selected_level(-1),
-	enemyLs(), playerLs(), enemyProjLs(), playerProjLs(), effectsLs()
+	enemyLs(new list<Entity*>), playerLs(new list<Entity*>), enemyProjLs(new list<Projectile*>), playerProjLs(new list<Projectile*>), effectsLs(new list<VisualEffect*>)
 {
 	if (!load_levels_from_file(level_path))
 		std::cerr << "Unable to load levels from: " << level_path << std::endl;
