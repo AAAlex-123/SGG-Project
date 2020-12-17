9246208620f527ed23be8df6f1ce5cc6bbe705e0
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-17 15:13:53 +0200

    U level.h: comments and updated interface

--- level.h
+++ level.h
@@ -6,27 +6,38 @@
 class Wave;
 class Spawnpoint;
 
+// defines a set of waves and their timings
 class Level
 {
 private:
 	const int _id;
 	const std::string _desc;
 
-	std::queue<std::pair<float, Wave*>*>* waves;
+	// queue of waves and their start time
+	// the time of each wave corresponds to the amount of time that will pass
+	// from level start before the wave starts
+	std::unordered_set<std::pair<float, Wave*>*>* waves;
+	// queue of enemies to be spawned (look at Wave::update() for why it is a queue)
+	std::queue<Entity*>* enemy_queue;
 
 	float _total_time;
 
 public:
 	Level(int, const std::string&);
 
+	// update level time and wave time
 	void update(float ms);
+	// delegate spawning to the waves
 	bool can_spawn();
 	Entity* spawn();
+
 	void add_wave(float, Wave*);
 
+	// used by level selection to display info and select level
 	int id() { return _id; }
 	std::string info() { return "level " + std::to_string(id()) + ": " + _desc; }
-	
+
+	// (wip) returns the string necessary to rebuild the level
 	std::string to_file_string();
 
 	~Level();
@@ -36,18 +47,24 @@ class Wave
 {
 private:
 	std::unordered_set<Spawnpoint*>* spawnpoints;
+	// queue of enemies to be spawned (look at Wave::update() for why it is a queue)
 	std::queue<Entity*>* enemy_queue;
 
 public:
 	Wave();
-	Wave(std::unordered_set<Spawnpoint*>* spawnpoints);
 
+	// update the spawnpoints' timer, add new enemies to enemy_queue
 	void update(float);
-
+	// work with the enemy_queue
 	bool can_spawn();
 	Entity* spawn();
+
 	void add_spawnpoint(Spawnpoint*);
 
+	// false if no spawnpoint has any enemies left
+	operator bool() const;
+
+	// (wip) returns the string necessary to rebuild the wave
 	std::string to_file_string();
 
 	~Wave();
@@ -56,18 +73,25 @@ public:
 class Spawnpoint
 {
 private:
+	// enemy parameters
 	const int type;
 	const float x, y, angle;
 
-	const float _duration, _spawn_delta, _initial_delay;
-	float _total_time, _elapsed_time;
+	// spawnpoint parameters
+	const float _spawn_delta;
+	int _amount;
+	float _initial_delay, _elapsed_time;
 
 public:
-	Spawnpoint(int type, float x, float y, float angle, float duration, float spawn_delta, float spawn_delay);
+	Spawnpoint(int type, float x, float y, float angle, int amount, float spawn_delta, float initial_delay);
 
 	void update(float);
 	bool can_spawn();
 	Entity* spawn();
 
+	// false if there are no enemies left
+	operator bool() const;
+
+	// (wip) returns the string necessary to rebuild the spawnpoint
 	std::string to_file_string();
 };
