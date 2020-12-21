06e8e681ab0a6bdd8dafda78ffb6bdd0214d9fde
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-21 22:43:38 +0200

    U entity.h: re-added my stuff again

--- entity.h
+++ entity.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "gameobject.h"
 
-//Entities really only differ in their movement and fire patterns. Since both can be described with already established algorithms (in the Path classes)
-//it's only logical we use those instead of subclassing every single enemy/player class. This is why we used composition (and subsequently a Factory class
-//that knows how to assemble each one).
+// Entities really only differ in their movement and fire patterns. Since both can be described with already established algorithms (in the Path classes)
+// it's only logical we use those instead of subclassing every single enemy/player class. This is why we used composition (and subsequently a Factory class
+// that knows how to assemble each one).
 
-class Projectile;	
+class Projectile;
 // VisualEffect is forward declared in gameobject.h
 
 //Interface for all interactive objects that can spawn other objects. Built using composition.
@@ -14,9 +14,10 @@ private:
 	int curr_projectile;
 	bool _hasFired;
 
-	Entity(float xpos, float ypos, float angle, float vel, float width,float height, const std::string* sprite_name, Path*,
-			int damage, int health, int score, int proj_type);
-	friend class Factory; //allow creation of entities only to the factory
+	Entity(float xpos, float ypos, float angle, float vel, float width, float height, const std::string* sprite_name, Path*,
+		int damage, int health, int score, int proj_type);
+
+	friend class GObjFactory; //allow creation of entities only to the factory
 	std::string shadow;
 
 public:
