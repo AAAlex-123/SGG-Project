9ac17cd1c1d232ed3a35d1b18e8697c2bd13a3bf
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-21 22:33:18 +0200

    U entity.h: re-added my stuff

--- entity.h
+++ entity.h
@@ -1,20 +1,21 @@
 #pragma once
 #include "gameobject.h"
-#include "projectile.h"
-#include <string>
 
 //Entities really only differ in their movement and fire patterns. Since both can be described with already established algorithms (in the Path classes)
 //it's only logical we use those instead of subclassing every single enemy/player class. This is why we used composition (and subsequently a Factory class
 //that knows how to assemble each one).
+
+class Projectile;	
+// VisualEffect is forward declared in gameobject.h
 
 //Interface for all interactive objects that can spawn other objects. Built using composition.
 class Entity : public GameObject {
 private:
 	int curr_projectile;
 	bool _hasFired;
-	Entity(float xpos, float ypos, float angle, float vel, float width,float height, const std::string* sprite_name, Path*,
-			int damage, int health, int score,int proj_type);
 
+	Entity(float xpos, float ypos, float angle, float vel, float width,float height, const std::string* sprite_name, Path*,
+			int damage, int health, int score, int proj_type);
 	friend class Factory; //allow creation of entities only to the factory
 	std::string shadow;
 
@@ -28,6 +29,8 @@ public:
 	virtual void draw() override;
 	// Returns a projectile of type `curr_projectile` using the friend GObjFactory class
 	Projectile* getProjectile() const;
+	VisualEffect* getFireVisualEffect() const;
+	VisualEffect* getDestructionVisualEffect() const override;
 
 	// Changes the projectile type
 	void setProjectile(int proj_type);
