b994a74c65322fcf7ea3fe7473eab9605363d46b
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-12-22 19:27:46 +0200

    UI & shadows impl (#29)
    * Add files via upload
    
    * Update entity.h
    
    added shadow drawing
    
    * Shadows impl
    
    * Added getSprites()
    
    * Brush->protected, draw nonfinal for shadow drawing
    
    * removed outline from drawings
    
    This is not the best way to do it but w/e
    
    * removed file extensions for entities
    
    * U entity.h: re-added my stuff
    
    * U entity: re-added my stuff
    
    * U gameobject: re-added my stuff
    
    * U drawing.h: re-added my stuff
    
    * U entity: re-added my stuff again
    
    * U entity.h: re-added my stuff again
    
    Co-authored-by: AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>

--- GObjFactory.cpp
+++ GObjFactory.cpp
@@ -22,19 +22,19 @@ Entity* GObjFactory::createEntity(int type, float x, float y, float angle, float
 }
 
 Entity* GObjFactory::createPlayer(float x, float y, float angle, float dangle, Keyset keyset) {
-	return new Entity(x, y, angle, std_speed / 2, (float)zep_width, (float)zep_height, new string(image_path + "player1.png"), new KeyboardPath(dangle, keyset), 5, 1, 0, GObjFactory::STANDARD_BULLET);
+	return new Entity(x, y, angle, std_speed / 2, (float)zep_width, (float)zep_height, new string(image_path + "player1"), new KeyboardPath(dangle, keyset), 5, 1, 0, GObjFactory::STANDARD_BULLET);
 }
 
 Entity* GObjFactory::createEnemy1(float x,float y,float angle) {
-	return new Entity(x, y, angle, std_speed, plane_size, plane_size, new string(image_path + "plane1.png"), new FiringPath(0.5f, new AcceleratingPath(150.0f, new Path())), 5, 1, 100, GObjFactory::STANDARD_BULLET);
+	return new Entity(x, y, angle, std_speed, plane_size, plane_size, new string(image_path + "plane1"), new FiringPath(0.5f, new AcceleratingPath(150.0f, new Path())), 5, 1, 100, GObjFactory::STANDARD_BULLET);
 }
 
 Entity* GObjFactory::createEnemy2(float x, float y, float angle) {
-	return new Entity(x, y, angle, std_speed, plane_size, plane_size, new string(image_path + "plane2.png"), new FiringPath(1.0f, new RotatingPath(1.0f / 4, new Path())), 5, 1, 150, GObjFactory::INCEND_BULLET);
+	return new Entity(x, y, angle, std_speed, plane_size, plane_size, new string(image_path + "plane2"), new FiringPath(1.0f, new RotatingPath(1.0f / 4, new Path())), 5, 1, 150, GObjFactory::INCEND_BULLET);
 }
 
 Entity* GObjFactory::createEnemy3(float x, float y, float angle) {
-	return new Entity(x, y, angle, std_speed, zep_width, zep_height, new string(image_path + "plane3.png"), new FiringPath(1.0f, new Path()), 5, 1, 400, GObjFactory::LIGHT_BULLET);
+	return new Entity(x, y, angle, std_speed, zep_width, zep_height, new string(image_path + "plane3"), new FiringPath(1.0f, new Path()), 5, 1, 400, GObjFactory::LIGHT_BULLET);
 }
 
 // ===== PROJECTILE =====
