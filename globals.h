1d05c5c09f0d1212e9aa627fd956a96c85d942c0
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-12-23 17:50:18 +0200

    Update globals.h

--- globals.h
+++ globals.h
@@ -15,9 +15,11 @@ struct Keyset
 // assets and other paths
 const std::string asset_path = ".\\assets\\";
 const std::string font = asset_path + "orange juice 2.0.ttf";
-const std::string image_path = ".\\assets\\";
+const std::string image_path = asset_path + "images\\";
+const std::string sound_path = asset_path + "sounds\\";
+const std::string music_path = asset_path + "music\\";
 const std::string level_path = ".\\levels.txt";
-const std::string sound_path = ".\\sounds\\";
+const std::string wave_path = ".\\waves.txt";
 
 float get_canvas_height();
 float get_canvas_width();
