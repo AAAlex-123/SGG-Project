5f56d41a002db263f3d8e4f041a777e9cda24c2d
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-23 14:39:06 +0200

    U UI.h: same as UI.cpp

--- UI.h
+++ UI.h
@@ -1,27 +1,22 @@
 #pragma once
-#include "entity.h"
-#include "graphics.h"
 #include "globals.h"
+#include "entity.h"
 #include "game_data.h"
 
-class UI{
-
+class UI {
 private:
 	bool isMult;
 	const float width, height;
-	Entity* player1; 
-	Entity* player2; //might be nullptr depending on initialization so not &
+	Entity** players;
 	graphics::Brush health_br, std_br, bg_br;
-	const GameData *const gd;
+	const GameData* const gd;
 	int new_fps = 40;
 
-	void drawPlayerPanel(Entity* player, float x_pos) ;
+	void drawPlayerPanel(const Entity* player, float x_pos);
 	void initializeBrushes();
 
 public:
-	UI(Entity* player, GameData* gd);
-	UI(Entity* player1, Entity* player2, GameData* gd);
+	UI(GameData* gd);
 
 	void draw();
 };
-
