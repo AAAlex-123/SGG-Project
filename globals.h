b490d600f916387477edff8eb3c5c989730ab8f9
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-12-24 17:24:05 +0200

    Update globals.h

--- globals.h
+++ globals.h
@@ -1,20 +1,15 @@
 #pragma once
+#include "graphics.h"
 #include "scancodes.h"
 #include <string>
+
 typedef graphics::scancode_t key;
 
 const float PI = 3.14159265359f;
 
-struct Keyset
-{
-	key up, down, left, right, rleft, rright, fire;
-	Keyset(key up, key down, key left, key right, key rleft, key rright, key fire)
-		: up(up), down(down), left(left), right(right), rleft(rleft), rright(rright), fire(fire) {}
-};
-
 // assets and other paths
 const std::string asset_path = ".\\assets\\";
-const std::string font = asset_path + "orange juice 2.0.ttf";
+const std::string font = asset_path + "standard_font.ttf";
 const std::string image_path = asset_path + "images\\";
 const std::string sound_path = asset_path + "sounds\\";
 const std::string music_path = asset_path + "music\\";
@@ -23,3 +18,13 @@ const std::string wave_path = ".\\waves.txt";
 
 float get_canvas_height();
 float get_canvas_width();
+
+void setColor(graphics::Brush&, const float*);
+void setColor(graphics::Brush&, char c);
+
+struct Keyset
+{
+	key up, down, left, right, rleft, rright, fire;
+	Keyset(key up, key down, key left, key right, key rleft, key rright, key fire)
+		: up(up), down(down), left(left), right(right), rleft(rleft), rright(rright), fire(fire) {}
+};
