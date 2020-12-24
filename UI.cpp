8c2ab4e9343348b04e37ab53b0a874e710757e32
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-12-24 17:50:09 +0200

    Update UI.cpp

--- UI.cpp
+++ UI.cpp
@@ -3,18 +3,13 @@
 UI::UI(GameData* gd) :
 	players(new Entity* [2]{ nullptr, nullptr }), width(get_canvas_width()), height(get_canvas_height()), gd(gd)
 {
-	int i = 0;
-	for (auto iter = gd->playerLs->begin(); iter != gd->playerLs->end(); ++iter) {
-		players[i] = *iter;
-		++i;
-	}
-
 	initializeBrushes();
 }
 
-
 inline void UI::initializeBrushes() {
-	setColor(health_br, new float[3]{ 1.f, 0.f, 0.f });
+	health_br.fill_color[0] = 1.f;
+	health_br.fill_color[1] = 0.f;
+	health_br.fill_color[2] = 0.f;
 
 	health_br.fill_secondary_color[0] = 0.f;
 	health_br.fill_secondary_color[1] = 1.f;
@@ -24,7 +19,13 @@ inline void UI::initializeBrushes() {
 	health_br.gradient_dir_v = 0.f;
 
 	bg_br.outline_opacity = 0.f;
+
+	std_br.fill_color[0] = 0.f;
+	std_br.fill_color[1] = 0.f;
+	std_br.fill_color[2] = 0.f;
 }
+
+
 
 void UI::drawPlayerPanel(const Entity* player, float x_pos) {
 
@@ -33,7 +34,7 @@ void UI::drawPlayerPanel(const Entity* player, float x_pos) {
 	//draw Text
 	char text[4] = { 'P', x_pos == 0.0f ? '1' : '2', ':', '\0' };	//:)
 	graphics::drawText(x_pos, height - 80, 20, text, std_br);
-	graphics::drawText(x_pos, height - 50, 20, "Fire:", std_br);
+	graphics::drawText(x_pos, height - 50, 20, "Bullets:", std_br);
 	graphics::drawText(x_pos, height - 15, 20, "HP", std_br);
 
 	//draw Player icon
@@ -46,7 +47,9 @@ void UI::drawPlayerPanel(const Entity* player, float x_pos) {
 	//draw Projectile icon
 	bg_br.texture = *(player->getProjectile()->getSprite());
 	graphics::setOrientation(0.f);
+	graphics::drawRect(x_pos + 90,  height - 50, 20, 40, bg_br);
 	graphics::drawRect(x_pos + 100, height - 50, 20, 40, bg_br);
+	graphics::drawRect(x_pos + 110, height - 50, 20, 40, bg_br);
 
 	//draw healthbar
 	health_br.outline_opacity = 0.f;
@@ -61,6 +64,11 @@ void UI::drawPlayerPanel(const Entity* player, float x_pos) {
 	graphics::drawRect(x_pos + 75, height - 20, 150, 20, health_br);
 }
 
+void UI::drawDestroyedPanel(float x_pos) {
+	//draw big-ass X
+	//draw default dead drawings
+}
+
 void UI::draw() {
 	graphics::setOrientation(0.f);
 	//draw UI box
@@ -68,10 +76,8 @@ void UI::draw() {
 	bg_br.texture = "";
 	graphics::drawRect(width / 2, height - 40, width, 110, bg_br);
 
-	setColor(std_br, 'G'); //green
-
 	//draw Score
-	graphics::drawText(width / 2 - 75, 60, 30, "PTS: " + std::to_string(gd->getScore()), std_br);
+	graphics::drawText(width / 2 - 75, 60, 30, "Score: " + std::to_string(gd->getScore()), std_br);
 
 	//draw FPS
 	if ((int)graphics::getGlobalTime() % 20 == 0) //change fps every 0.2 seconds
@@ -82,7 +88,7 @@ void UI::draw() {
 	graphics::drawText(width / 75, 2 * width / 20 + 20, ((width + height) / 2) / 35, "Level " + std::to_string(-gd->curr_playing_level - 1), std_br);
 
 	//draw Panels
-	drawPlayerPanel(players[0], 0);
-	if (players[1] != nullptr)
-		drawPlayerPanel(players[1], width - 175);
+	drawPlayerPanel(gd->playerLs->front(), 0);
+	if (gd->playerLs->size()>1)
+		drawPlayerPanel(gd->playerLs->back(), width - 175);
 }
