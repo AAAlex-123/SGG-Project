ee5949253d0a10d1f74227ec9ff288d738f54ce7
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2020-12-27 14:52:54 +0200

    U Path: added homing Path, bugfixes
    targeted firing path is the same as path just a different class so they can be differentiated

--- Path.cpp
+++ Path.cpp
@@ -1,8 +1,13 @@
 #include "Path.h"
+#include "drawing.h"
 #include <cmath>
 
 bool Path::move(float& x, float& y, float& angle, float& vel, float ms)
 {
+	if (angle > 180.0f)
+		angle -= 360.0f;
+	else if (angle < -180.0f)
+		angle += 360.0f;
 	x -= vel * (float)sin(angle) * (ms / 1000.0f);
 	y -= vel * (float)cos(angle) * (ms / 1000.0f);
 	return false;
@@ -11,15 +16,15 @@ bool Path::move(float& x, float& y, float& angle, float& vel, float ms)
 bool AcceleratingPath::move(float& x, float& y, float& angle, float& vel, float ms)
 {
 	vel += dvel * (ms / 1000.0f);
-	_path->move(x, y, angle, vel, ms);
-	return false;
+
+	return _path->move(x, y, angle, vel, ms);
 }
 
 bool RotatingPath::move(float& x, float& y, float& angle, float& vel, float ms)
 {
 	angle += dangle * 2 * PI * (ms / 1000.0f);
-	_path->move(x, y, angle, vel, ms);
-	return false;
+
+	return _path->move(x, y, angle, vel, ms);
 }
 
 bool KeyboardPath::move(float& x, float& y, float& angle, float& vel, float ms)
@@ -45,7 +50,7 @@ bool KeyboardPath::move(float& x, float& y, float& angle, float& vel, float ms)
 		x += vel * (float)sin(angle + PI / 2) * (ms / 1000.0f);
 		y += vel * (float)cos(angle + PI / 2) * (ms / 1000.0f);
 	}
-	// decrease or keep at 0
+	// decrease cooldown or keep at 0
 	remaining = ((0.0f) * (remaining <= 0.0f)) + ((remaining - (ms / 1000.0f)) * (remaining > 0.0f));
 	// reset if it is 0 and fire is being pressed
 	remaining = ((cooldown) * (remaining == 0.0f && graphics::getKeyState(keyset.fire))) + ((remaining) * !(remaining == 0.0f && graphics::getKeyState(keyset.fire)));
@@ -56,12 +61,37 @@ bool KeyboardPath::move(float& x, float& y, float& angle, float& vel, float ms)
 bool FiringPath::move(float& x, float& y, float& angle, float& vel, float ms)
 {
 	elapsed += (ms / 1000.0f);
-	float e = elapsed;
-	elapsed = (0 * (elapsed >= period)) + (elapsed * (elapsed < period));
+	elapsed = (0.0f * (elapsed >= period)) + (elapsed * (elapsed < period));
 
 	_path->move(x, y, angle, vel, ms);
 
-	return e >= period;
+	return elapsed == 0.0f;
 }
 
-bool StaticPath::move(float& x, float& y, float& angle, float& vel, float ms) { return false; }
+bool TargetedFiringPath::move(float& x, float& y, float& angle, float& vel, float ms)
+{
+	elapsed += (ms / 1000.0f);
+	elapsed = (0.0f * (elapsed >= period)) + (elapsed * (elapsed < period));
+
+	_path->move(x, y, angle, vel, ms);
+
+	return elapsed == 0.0f;
+}
+
+bool HomingPath::move(float& x, float& y, float& angle, float& vel, float ms)
+{
+	// mafs
+
+	float slope = atan2((followee->get_x() - x), (followee->get_y() - y));
+
+	float dangle = -(angle - slope + PI);
+
+	angle += (dangle < -PI) ? ((dangle + 2 * PI) * perc) : (dangle * perc);
+
+	return _path->move(x, y, angle, vel, ms);
+}
+
+bool StaticPath::move(float& x, float& y, float& angle, float& vel, float ms)
+{
+	return false;
+}
