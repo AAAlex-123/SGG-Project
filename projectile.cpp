991bbf38450df5dfe6710ac66de985925f3b32fd
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-12-27 18:58:43 +0200

    Added sound support for hitting enemies

--- projectile.cpp
+++ projectile.cpp
@@ -1,5 +1,7 @@
 #include "projectile.h"
 #include "GObjFactory.h"
+#include "graphics.h"
+#include "globals.h"
 
 Projectile::Projectile(float xpos, float ypos, float angle, float vel, float radius, std::string* sprites, Path* p, int damage) :
 	GameObject(xpos, ypos, angle, vel, radius,radius, sprites, p, damage, 1,0) //width == height -> square
@@ -7,6 +9,11 @@ Projectile::Projectile(float xpos, float ypos, float angle, float vel, float rad
 
 void Projectile::update(float ms){
 	this->movement->move(this->x, this->y, this->angle, this->vel, ms);
+}
+
+void Projectile::hit(const GameObject& o2) {
+	GameObject::hit(o2);
+	graphics::playSound(sound_path + "hit.mp3", 0.2f, false);
 }
 
 VisualEffect* Projectile::getDestructionVisualEffect() const {
