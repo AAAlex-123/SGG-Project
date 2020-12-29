05258b916f96b1cdac69a4ee48abf5a484161e00
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2020-12-29 18:56:09 +0200

    Update projectile.cpp

--- projectile.cpp
+++ projectile.cpp
@@ -5,13 +5,15 @@
 
 Projectile::Projectile(float xpos, float ypos, float angle, float vel, float radius, std::string* sprites, Path* p, int damage) :
 	GameObject(xpos, ypos, angle, vel, radius,radius, sprites, p, damage, 1,0) //width == height -> square
-{}
+{
+	graphics::playSound(sound_path + "fire.mp3", 0.2f, false);
+}
 
 void Projectile::update(float ms){
 	this->movement->move(this->x, this->y, this->angle, this->vel, ms);
 }
 
-void Projectile::hit(const GameObject& o2) {
+void Projectile::hit(GameObject* o2) {
 	GameObject::hit(o2);
 	graphics::playSound(sound_path + "hit.mp3", 0.2f, false);
 }
