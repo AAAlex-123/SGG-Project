12191453628e7a7c650cdaa1d91de1d2071a746d
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-01-02 16:53:16 +0200

    U entity.h: same as entity.cpp

--- entity.h
+++ entity.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "gameobject.h"
-#include <list>
 
 // Entities really only differ in their movement and fire patterns. Since both can be described with already established algorithms (in the Path classes)
 // it's only logical we use those instead of subclassing every single enemy/player class. This is why we used composition (and subsequently a Factory class
@@ -13,11 +12,10 @@ class Projectile;
 class Entity : public GameObject {
 protected:
 	int curr_projectile;
-	bool targeting;
 	bool _hasFired;
 
 	Entity(float xpos, float ypos, float angle, float vel, float width, float height, const std::string* sprite_name, FiringPath*,
-		int damage, int health, int score, int proj_type,bool can_target = false);
+		int damage, int health, int score, int proj_type);
 	friend class GObjFactory; //allow creation of entities only to the factory
 
 	std::string shadow;
