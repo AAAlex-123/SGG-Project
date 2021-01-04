70cbe1b9a9348587cbbe1530903b3201315daa42
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-01-04 10:20:38 +0200

    U constants.h: no const, game_states to globals.h

--- constants.h
+++ constants.h
@@ -2,33 +2,15 @@
 #include "graphics.h"
 
 // constants
-const short WINDOW_WIDTH = 1200;
-const short WINDOW_HEIGHT = 600;
+short WINDOW_WIDTH = 1200;
+short WINDOW_HEIGHT = 600;
 const float CANVAS_WIDTH = 400.0f;
 const float CANVAS_HEIGHT = 500.0f;
-// maps window to canvas or the inverse; used at the end of main.cpp
-const float w2c = CANVAS_HEIGHT / WINDOW_HEIGHT;
-const float c2w = 1.0f / w2c;
-
-// enum
-enum game_states
-{
-	TEST = -2,
-	LOAD = -1,
-	CREDITS = 0,
-	MENU = 1,
-	GAME = 2,
-	GAME_LOSE = 10,
-	GAME_WIN = 11,
-	OPTIONS = 4,
-	OP_LEVEL = 5,
-	OP_PLAYER = 6,
-	LEVEL_TRANSITION = 7,
-	HELP = 8,
-	EXIT = 9,
-	PAUSE = 12,
-	RESET = 13,
-};
+// maps window to canvas or the inverse; used for mouse calculations
+float w2c = (WINDOW_HEIGHT - CANVAS_HEIGHT) < (WINDOW_WIDTH - CANVAS_WIDTH)
+	? CANVAS_HEIGHT / WINDOW_HEIGHT
+	: CANVAS_WIDTH / WINDOW_WIDTH;
+float c2w = 1.0f / w2c;
 
 // function declarations
 void initialize();
