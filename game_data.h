750d0f8afac882014466affc3bd3a77039a4c6e3
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-01-09 18:46:05 +0200

    U game_data.h: updated next level
    successive level id's now go up by one

--- game_data.h
+++ game_data.h
@@ -65,11 +65,11 @@ public:
 	int curr_playing_level;
 	float level_transition_timer;
 	float set_level_transition_timer(float timer = 5.0f) { return timer; }
-	void next_level() { --curr_playing_level; }
+	void next_level() { ++curr_playing_level; }
 	Level* has_next_level() { 
-		Level* return_val = levels[curr_playing_level - 1];
+		Level* return_val = levels[curr_playing_level + 1];
 		if (!return_val)
-			levels.erase(curr_playing_level - 1);
+			levels.erase(curr_playing_level + 1);
 		return return_val;
 	}
 
