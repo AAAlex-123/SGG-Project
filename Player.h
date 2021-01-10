e6699fe319b54497712676ec31198f38ae042f1f
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-01-10 14:22:16 +0200

    Player has KeyboardPath, methods moved to cpp

--- Player.h
+++ Player.h
@@ -1,31 +1,39 @@
 #pragma once
+#include "Path.h"
 #include "entity.h"
 #include "globals.h"
 #include "GObjFactory.h"
 #include <iostream>
 
+//A subclass of Entity that can interact with powerups and has access to a unique path.
 class Player final : public Entity{
+private:
+    // Standalone Path that responds to Keyboard
+    class KeyboardPath : public FiringPath {
+    public:
+        KeyboardPath(float dangle, float cooldown, Keyset keyset) : FiringPath(cooldown, this), dangle(dangle), remaining(0.0f), keyset(keyset)
+        {}
+        virtual bool move(float& x, float& y, float& angle, float& vel, float ms) override;
+
+    private:
+        // % of a full rotation per second
+        const float dangle;
+        float remaining;
+        const Keyset keyset;
+    };
 
 public:
 
-	Player(float xpos, float ypos, float angle, float vel, float width, float height, const std::string* sprite_name, float dangle,float cooldown, Keyset keys,
-		int health, int proj_type) :
-		Entity(xpos, ypos, angle, vel, width, height, sprite_name, new KeyboardPath(dangle, cooldown, keys),INT_MAX, health,0, proj_type)
-	{}
+    Player(float xpos, float ypos, float angle, float vel, float width, float height, const std::string* sprite_name, float dangle, float cooldown, Keyset keys,
+        int health, int proj_type);
 
 	// Changes the projectile type
-	void incrementProjectile() {
-		if (curr_projectile == GObjFactory::LIGHT_BULLET)
-			curr_projectile = GObjFactory::STANDARD_BULLET;
-		else if (curr_projectile == GObjFactory::STANDARD_BULLET)
-			curr_projectile = GObjFactory::HEAVY_BULLET;
-	}
-
-	void addHealth(int health) {
-		curr_health += health;
-
-		max_health = curr_health > max_health ? curr_health : max_health;
-	}
+    void incrementProjectile();
+    //Adds health, by sometimes buffing the max health
+    void addHealth(int health);
 
 	virtual ~Player() = default;
 };
+
+
+
