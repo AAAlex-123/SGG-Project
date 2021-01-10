e3c465c19ec8372d4654ab0a41915e3fbcc9da06
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-01-10 21:49:17 +0200

    U constants.h: made prettier

--- constants.h
+++ constants.h
@@ -5,32 +5,24 @@
 #include <iostream>
 #include <regex>
 
-//A file dedicated to store the numerous constants and functions main.cpp uses
+/**
+* A file dedicated to storing numerous constants and functions that main.cpp uses
+*/
 
 // constants
-short WINDOW_WIDTH = 1200;
-short WINDOW_HEIGHT = 600;
-const float CANVAS_WIDTH = 400.0f;
+int WINDOW_WIDTH  = 1200;
+int WINDOW_HEIGHT = 600;
+const float CANVAS_WIDTH  = 400.0f;
 const float CANVAS_HEIGHT = 500.0f;
-// maps window to canvas or the inverse; used for mouse calculations
+
+// constants to match window dimensions to canvas dimensions; used for mouse calculations
 float w2c = (WINDOW_HEIGHT - CANVAS_HEIGHT) < (WINDOW_WIDTH - CANVAS_WIDTH)
 	? CANVAS_HEIGHT / WINDOW_HEIGHT
 	: CANVAS_WIDTH / WINDOW_WIDTH;
 float c2w = 1.0f / w2c;
 
-// function declarations
+// functions
 void initialize();
-bool load_images_from_file(const std::string& image_path);
-
-// key short names
-const graphics::scancode_t K_UP = graphics::scancode_t::SCANCODE_UP;
-const graphics::scancode_t K_DOWN = graphics::scancode_t::SCANCODE_DOWN;
-const graphics::scancode_t K_LEFT = graphics::scancode_t::SCANCODE_LEFT;
-const graphics::scancode_t K_RIGHT = graphics::scancode_t::SCANCODE_RIGHT;
-const graphics::scancode_t K_W = graphics::scancode_t::SCANCODE_W;
-const graphics::scancode_t K_A = graphics::scancode_t::SCANCODE_A;
-const graphics::scancode_t K_S = graphics::scancode_t::SCANCODE_S;
-const graphics::scancode_t K_D = graphics::scancode_t::SCANCODE_D;
 
 bool load_images_from_file(const std::string& image_path)
 {
@@ -55,7 +47,7 @@ bool load_images_from_file(const std::string& image_path)
 
 	while (std::regex_search(contents, match, r))
 	{
-		gd->images.push_back(match[1].str());
+		gd->image_names.push_back(match[1].str());
 		contents = match.suffix();
 	}
 
