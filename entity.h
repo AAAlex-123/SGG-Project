2a25485f3e475c8f703c1e733b0a9f5cff2ea931
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-01-10 21:50:38 +0200

    U entity.h: made prettier

--- entity.h
+++ entity.h
@@ -1,37 +1,40 @@
 #pragma once
 #include "gameobject.h"
 
-// Entities really only differ in their movement and fire patterns. Since both can be described with already established algorithms (in the Path classes)
-// it's only logical we use those instead of subclassing every single enemy/player class. This is why we used composition (and subsequently a Factory class
-// that knows how to assemble each one).
-
 class Projectile;
-// VisualEffect is forward declared in gameobject.h
 
-//Interface for all interactive objects that can spawn other objects. Built using composition.
-class Entity : public GameObject {
-protected:
-	int curr_projectile;
-	bool _hasFired;
+// Entities really only differ in their movement and fire patterns. Since both can be described with already established algorithms
+// (defined in the Path classes) it's only logical we use those instead of subclassing every single enemy/player class.
+// This is why we used composition (and subsequently a Factory  that knows how to assemble each one).
 
-	Entity(float xpos, float ypos, float angle, float vel, float width, float height, const std::string* const sprite_name, Path*,
-		int damage, int health, int score, int proj_type);
-	friend class GObjFactory; //allow creation of entities only to the factory
-
-	std::string shadow;
-
+/**
+* Interface for all interactive objects that can spawn other objects. Built using composition.
+*/
+class Entity : public GameObject
+{
 public:
-	// Routine called by the update() main function; updated the object's state
 	virtual void update(float ms) override;
 
-	// Returns whether or not this object has fired
 	bool hasFired() const;
 
+	// override draw() to add shadows
 	virtual void draw() override;
+
 	// Returns a projectile of type `curr_projectile` using the friend GObjFactory class
 	Projectile* getProjectile() const;
 	VisualEffect* getFireVisualEffect() const;
 	VisualEffect* getDestructionVisualEffect() const override;
 
 	virtual ~Entity();
+
+protected:
+	int curr_projectile;
+	bool _hasFired;
+
+	Entity(float xpos, float ypos, float angle, float vel, float width, float height, const std::string* const sprite_name, Path*,
+		int damage, int health, int score, int proj_type);
+
+	friend class GObjFactory; // allow creation of entities only to the factory
+
+	std::string shadow;
 };
