fd4d576662f11650056da8df06baf42c0afd1634
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-01-10 21:49:54 +0200

    U drawing.h: made prettier

--- drawing.h
+++ drawing.h
@@ -2,33 +2,36 @@
 #include "globals.h"
 #include "Path.h"
 
-//Interface to allow uniform access to all drawable objects. Includes a standard virtual drawing method.
-//A Drawing is any object that can be drawn and move on the screen.
-class Drawing {
-
-protected:
-	static graphics::Brush br;
-	const std::string* const sprites; //this could either be an array or a single string, internally it's handled the same way
-
-	float x, y, angle, vel;	// angle is measured in radiants and not in degrees
-	const float width, height;
-	int curr_sprite;
-
-	Path* movement; //movement across the screen
-
+/**
+* Interface to allow uniform access to all drawable objects. Includes a standard virtual drawing method.
+* A Drawing is any object that can be drawn and can move on the screen.
+*/
+class Drawing
+{
 public:
 	Drawing(float xpos, float ypos, float angle, float vel, float width, float height, const std::string* const sprites, Path* = new StaticPath());
 
-	// Updates the entities state (movement etc.)
+	// Updates the entity's state
 	virtual void update(float) = 0;
-	// Draws the object at [x, y] with `angle` orientation with its `curr_sprite`
 	virtual void draw();
 
 	// Returns false if the object is not alive; i.e. it must be destroyed
 	virtual operator bool() const = 0;
 
-	float get_x() const { return x; }
-	float get_y() const { return y; }
+	inline float get_x() const { return x; }
+	inline float get_y() const { return y; }
 
 	virtual ~Drawing();
+
+protected:
+	static graphics::Brush br;
+
+	// the set of the object's sprites. all objects except VisualEffects use just one sprite
+	const std::string* const sprites; // this could either be an array or a single string, internally it's handled the same way
+
+	float x, y, angle, vel;	// angle is measured in radiants, not in degrees
+	const float width, height;
+	int curr_sprite;
+
+	Path* const movement;
 };
