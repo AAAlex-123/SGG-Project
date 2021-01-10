8a81fe435cb53f417708521a8ee79946fa4efd3d
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-01-10 21:48:53 +0200

    U Button.h: made prettier

--- button.h
+++ button.h
@@ -4,55 +4,60 @@
 
 class GameData;
 
+/**
+* Αn interface for buttons that, when clicked, perform an action
+*/
 class Button : public Drawing
 {
-protected:
-	GameData* gd;
-	int targetGS;
-	bool clicked;
-	graphics::MouseState ms;
-	
-	// this one is specified by subclasses
-	virtual void execute_() const = 0;
-
 public:
 	Button(GameData*, float, float, float, std::string*, int targetGS);
 
 	virtual void update(float) override final;
 	virtual void draw() override final;
 
-	// this one is called
+	// template method: this called by other classes
 	virtual void execute() const final;
 
-	// so inheritance works lmao
-	virtual operator bool() const override { return true; }
+	// defined
+	virtual operator bool() const override;
+
+protected:
+	GameData* gd;
+	int targetGS;	// the gamestate where the button will work
+	bool clicked;
+	graphics::MouseState ms;
+	
+	// template method: this one is specified by subclasses
+	virtual void execute_() const = 0;
 };
 
 class GameStateChangingButton : public Button
 {
-private:
-	int newGS;
-
 public:
 	GameStateChangingButton(GameData*, float, float, float, std::string*, int, int newGS);
 
+protected:
 	virtual void execute_() const override;
+
+private:
+	int newGS;
 };
 
 template<typename T>
 class VariableChangingButton : public Button
 {
-private:
-	T* attr;
-	T val;
-
 public:
 	VariableChangingButton(GameData* gd, float x, float y, float size, std::string* icon, int targetGS, T* attribute, T value)
 		: Button(gd, x, y, size, icon, targetGS), attr(attribute), val(value)
 	{}
 
+protected:
 	virtual void execute_() const override
 	{
 		*attr = val;
 	}
+
+private:
+	T* attr;
+	T val;
 };
