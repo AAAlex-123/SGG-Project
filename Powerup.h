8118d2ba4f58dd4a99539dc293898784597ce881
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-01-10 16:07:01 +0200

    Descriptions

--- Powerup.h
+++ Powerup.h
@@ -4,14 +4,18 @@
 
 class Player;
 
+//A superclass defining objects that when interact with a player give him any kind of buff
+//Subclasses specify the details of said buff
 class Powerup :public GameObject {
 protected:
+	//A simple template method called whenever a powerup is touched
 	virtual void hit(GameObject* o2) final override;
 
 public:
 	Powerup(float xpos, float ypos, float angle, const std::string* sprite, int score);
-
+	//The method subclasses override to specify hit()'s behavior
 	virtual void consume(Player* target) const = 0;
+	//Powerup is implemented with the clone pattern because of how levels are loaded
 	virtual Powerup* clone() const = 0;
 
 	virtual VisualEffect* getDestructionVisualEffect() const override;
