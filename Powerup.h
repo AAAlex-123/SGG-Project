06ce95bd4da4bdb94824bf763777cc28d6e95dfc
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-01-11 17:57:32 +0200

    U Powerup.h: same as Powerup.cpp

--- Powerup.h
+++ Powerup.h
@@ -1,18 +1,17 @@
 #pragma once
-#include "globals.h"
 #include "gameobject.h"
 
 class Player;
 
-//A superclass defining objects that when interact with a player give him any kind of buff
-//Subclasses specify the details of said buff
-class Powerup :public GameObject {
-protected:
-	//A simple template method called whenever a powerup is touched
-	virtual void hit(GameObject* o2) final override;
-
+/**
+ * A superclass defining objects that when interact with a player give him any kind of buff
+ * Subclasses specify the details of said buff
+ */ 
+class Powerup :public GameObject
+{
 public:
 	Powerup(float xpos, float ypos, float angle, const std::string* sprite, int score);
+
 	//The method subclasses override to specify hit()'s behavior
 	virtual void consume(Player* target) const = 0;
 	//Powerup is implemented with the prototype pattern because of how they get spawned from Level
@@ -21,12 +20,14 @@ public:
 	virtual VisualEffect* getDestructionVisualEffect() const override;
 
 	virtual ~Powerup();
+
+protected:
+	// A simple template method called whenever a powerup is touched
+	virtual void hit(GameObject* o2) final override;
 };
 
-class HealthPowerup : public Powerup {
-private:
-	static const int score = 10000;
-	const std::string sprite = std::string(image_path + "h_powerup.png");
+class HealthPowerup : public Powerup
+{
 public:
 
 	HealthPowerup(float xpos, float ypos, float angle);
@@ -36,14 +37,13 @@ public:
 	virtual Powerup* clone() const;
 
 	virtual ~HealthPowerup() = default;
-};
 
-class ProjectilePowerup : public Powerup {
 private:
 	static const int score = 10000;
-	const std::string sprite = std::string(image_path + "proj_powerup.png");
-	int proj_type = 0;
+};
 
+class ProjectilePowerup : public Powerup
+{
 public:
 	ProjectilePowerup(float xpos, float ypos, float angle);
 
@@ -52,13 +52,13 @@ public:
 	virtual Powerup* clone() const;
 
 	virtual ~ProjectilePowerup() = default;
+
+private:
+	static const int score = 10000;
 };
 
-class PointsPowerup : public Powerup {
-private:
-	static const int score = 100000;
-	const std::string sprite = std::string(image_path + "points_powerup.png");
-
+class PointsPowerup : public Powerup
+{
 public:
 	PointsPowerup(float xpos, float ypos, float angle);
 
@@ -67,4 +67,7 @@ public:
 	virtual Powerup* clone() const;
 
 	virtual ~PointsPowerup() = default;
+
+private:
+	static const int score = 100000;
 };
