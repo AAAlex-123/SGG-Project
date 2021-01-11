835f2d3e4606ffc32d172598a601fce4abcfad09
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-01-11 18:06:32 +0200

    U visual_effect: made prettier

--- visual_effect.cpp
+++ visual_effect.cpp
@@ -4,29 +4,32 @@
 VisualEffect::VisualEffect(float xpos, float ypos, float angle, float vel, float size, std::string* sprites,
 	int sprite_no, float duration, float fps) :
 	Drawing(xpos, ypos, angle, vel, size, size, sprites, new StaticPath),
-	sprite_no(sprite_no), duration(duration), spf(1 / fps), elapsed(0), total_elapsed(0)
+	sprite_no(sprite_no), duration(duration), sprites_per_second(1 / fps), elapsed(0), total_elapsed(0)
 {}
 
-// Returns false if the object is not alive; i.e. it must be destroyed
-VisualEffect::operator bool() const {
-	return total_elapsed < duration;
-}
-
 // Moved the effect and changes the current sprite based on the elapsed time
-void VisualEffect::update(float ms) {
+void VisualEffect::update(float ms)
+{
 	this->movement->move(x, y, angle, vel, ms);
 
 	total_elapsed += ms / 1000.0f;
 	elapsed += ms / 1000.0f;
 
-	curr_sprite = (((curr_sprite + 1) % sprite_no) * (elapsed > spf)) + (curr_sprite * !(elapsed > spf));
-	elapsed = (0.0f * (elapsed > spf)) + (elapsed * !(elapsed > spf));
+	curr_sprite = (((curr_sprite + 1) % sprite_no) * (elapsed > sprites_per_second)) + (curr_sprite * !(elapsed > sprites_per_second));
+	elapsed = (0.0f * (elapsed > sprites_per_second)) + (elapsed * !(elapsed > sprites_per_second));
 }
 
-VisualEffect* VisualEffect::getDestructionVisualEffect() const {
+VisualEffect::operator bool() const
+{
+	return total_elapsed < duration;
+}
+
+VisualEffect* VisualEffect::getDestructionVisualEffect() const
+{
 	return GObjFactory::createVisualEffect(GObjFactory::EFFECT::NOEFFECT, 0, 0, 0, 0, 0);
 }
 
-VisualEffect::~VisualEffect() {
+VisualEffect::~VisualEffect()
+{
 	delete[] sprites;
 }
