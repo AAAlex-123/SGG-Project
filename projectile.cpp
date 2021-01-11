a65408892ea7d65e1f2b3ac19dc64daa2fdb67c7
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-01-11 18:03:15 +0200

    U Projectile: made prettier

--- projectile.cpp
+++ projectile.cpp
@@ -1,22 +1,23 @@
 #include "projectile.h"
 #include "GObjFactory.h"
 
-Projectile::Projectile(float xpos, float ypos, float angle, float vel, float radius, std::string* sprites, Path* p, int damage) :
-	GameObject(xpos, ypos, angle, vel, radius, radius, sprites, p, damage, 1, 0) //width == height -> square
+Projectile::Projectile(float xpos, float ypos, float angle, float vel, float radius, std::string* sprites, Path* p, int damage)
+	: GameObject(xpos, ypos, angle, vel, radius, radius, sprites, p, damage, 1, 0)
 {}
 
 
-void Projectile::update(float ms){
+void Projectile::update(float ms)
+{
 	this->movement->move(this->x, this->y, this->angle, this->vel, ms);
 }
 
-void Projectile::hit(GameObject* o2) {
+void Projectile::hit(GameObject* o2)
+{
 	GameObject::hit(o2);
 	graphics::playSound(sound_path + "hit.mp3", 0.2f, false);
 }
 
-VisualEffect* Projectile::getDestructionVisualEffect() const {
+VisualEffect* Projectile::getDestructionVisualEffect() const
+{
 	return GObjFactory::createVisualEffect(GObjFactory::EFFECT::NOEFFECT, 0, 0, 0, 0, 0);
 }
-
-Projectile::~Projectile() {}
