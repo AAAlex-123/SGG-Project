02bb61a0e11472562e103b01d862c15112f1e9d6
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-01-11 17:56:22 +0200

    U Player.h: made prettier

--- Player.h
+++ Player.h
@@ -3,14 +3,28 @@
 
 class FiringPath;
 
-//A subclass of Entity that can interact with powerups and has access to a unique path.
-class Player final : public Entity{
+/**
+ * A subclass of Entity that can interact with powerups and has access to a unique path.
+ */
+class Player final : public Entity
+{
+public:
+    Player(float xpos, float ypos, float angle, float vel, float width, float height, const std::string* sprite_name, float dangle, float cooldown,
+        Keyset keys, int health, GObjFactory::BULLET proj_type);
+
+    // Changes the projectile type
+    void upgradeProjectile();
+    // Adds health, sometimes also buffs the max health
+    void addHealth(int health);
+
+    virtual ~Player() = default;
+
 private:
     // Standalone Path that responds to Keyboard
-    class KeyboardPath : public FiringPath {
+    class KeyboardPath : public FiringPath
+    {
     public:
-        KeyboardPath(float dangle, float cooldown, Keyset keyset) : FiringPath(cooldown, this), dangle(dangle), remaining(0.0f), keyset(keyset)
-        {}
+        KeyboardPath(float dangle, float cooldown, Keyset keyset);
         virtual bool move(float& x, float& y, float& angle, float& vel, float ms) override;
 
     private:
@@ -19,16 +33,4 @@ private:
         float remaining;
         const Keyset keyset;
     };
-
-public:
-
-    Player(float xpos, float ypos, float angle, float vel, float width, float height, const std::string* sprite_name, float dangle, float cooldown, Keyset keys,
-        int health, GObjFactory::BULLET proj_type);
-
-	// Changes the projectile type
-    void incrementProjectile();
-    //Adds health, by sometimes buffing the max health
-    void addHealth(int health);
-
-	virtual ~Player() = default;
 };
