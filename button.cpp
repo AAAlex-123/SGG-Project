cee38e7c52e717aeaa12f818c5047ad549eedb63
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-01-13 20:25:17 +0200

    const

--- button.cpp
+++ button.cpp
@@ -1,7 +1,7 @@
 #include "button.h"
 #include "game_data.h"
 
-Button::Button(GameData* gd, float x, float y, float size, std::string* icon, GAME_STATE targetGS)
+Button::Button(GameData* const gd, float x, float y, float size, std::string* const icon, GAME_STATE targetGS)
 	: Drawing(x, y, 0, 0, size, size, icon), gd(gd), targetGS(targetGS), clicked(false), ms(graphics::MouseState())
 {}
 
@@ -34,7 +34,7 @@ Button::operator bool() const
 }
 
 // GameStateChangingButton
-GameStateChangingButton::GameStateChangingButton(GameData* gd, float x, float y, float size, std::string* icon, GAME_STATE targetGS, GAME_STATE newGS)
+GameStateChangingButton::GameStateChangingButton(GameData* const gd, float x, float y, float size, std::string* const icon, GAME_STATE targetGS, GAME_STATE newGS)
 	: Button(gd, x, y, size, icon, targetGS), newGS(newGS)
 {}
 
