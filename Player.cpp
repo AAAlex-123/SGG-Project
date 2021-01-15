2013f805e926efd1eb38e6a9a764440864c0d58d
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-01-15 12:57:01 +0200

    U Player: factory rename

--- Player.cpp
+++ Player.cpp
@@ -6,16 +6,16 @@
 #include <climits>
 
 Player::Player(float xpos, float ypos, float angle, float vel, float width, float height, const std::string* sprite_name, float dangle, float cooldown, Keyset keys,
-    int health, GObjFactory::BULLET proj_type)
+    int health, Factory::BULLET proj_type)
     : Entity(xpos, ypos, angle, vel, width, height, sprite_name, new KeyboardPath(dangle, cooldown, keys), INT_MAX, health, 0, proj_type)
 {}
 
 void Player::upgradeProjectile()
 {
-    if (curr_projectile == GObjFactory::BULLET::LIGHT_BULLET)
-        curr_projectile = GObjFactory::BULLET::STANDARD_BULLET;
-    else if (curr_projectile == GObjFactory::BULLET::STANDARD_BULLET)
-        curr_projectile = GObjFactory::BULLET::HEAVY_BULLET;   
+    if (curr_projectile == Factory::BULLET::LIGHT_BULLET)
+        curr_projectile = Factory::BULLET::STANDARD_BULLET;
+    else if (curr_projectile == Factory::BULLET::STANDARD_BULLET)
+        curr_projectile = Factory::BULLET::HEAVY_BULLET;   
 }
 
 void Player::addHealth(int health)
