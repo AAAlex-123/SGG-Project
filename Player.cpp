a5352ba7892f73edd7818d3e501d779607fb83c2
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-01-15 12:39:01 +0200

    U Player: renaming and includes
    to please the compiler and gcc includes

--- Player.cpp
+++ Player.cpp
@@ -2,6 +2,8 @@
 #include "GObjFactory.h"
 #include "Path.h"
 #include "UI.h" //access to UI's "hitbox"
+#include <cmath>
+#include <climits>
 
 Player::Player(float xpos, float ypos, float angle, float vel, float width, float height, const std::string* sprite_name, float dangle, float cooldown, Keyset keys,
     int health, GObjFactory::BULLET proj_type)
@@ -25,45 +27,45 @@ void Player::addHealth(int health)
 Player::KeyboardPath::KeyboardPath(float dangle, float cooldown, Keyset keyset) : FiringPath(cooldown, this), dangle(dangle), remaining(0.0f), keyset(keyset)
 {}
 
-bool Player::KeyboardPath::move(float& x, float& y, float& angle, float& vel, float ms)
+bool Player::KeyboardPath::move(float& pl_x, float& pl_y, float& pl_angle, float& pl_vel, float ms)
 {
     // angle
-    if (graphics::getKeyState(keyset.rleft)) angle += dangle * 2 * PI * (ms / 1000.0f);
-    if (graphics::getKeyState(keyset.rright)) angle -= dangle * 2 * PI * (ms / 1000.0f);
+    if (graphics::getKeyState(keyset.rleft)) pl_angle += dangle * 2 * PI * (ms / 1000.0f);
+    if (graphics::getKeyState(keyset.rright)) pl_angle -= dangle * 2 * PI * (ms / 1000.0f);
     
-    this->curr_angle = angle;
+    this->curr_angle = pl_angle;
 
     // position
-    float temp_x = x;
-    float temp_y = y;
+    float temp_x = pl_x;
+    float temp_y = pl_y;
 
     if (graphics::getKeyState(keyset.up)) {
-        temp_x -= vel * (float)sin(angle) * (ms / 1000.0f);
-        temp_y -= vel * (float)cos(angle) * (ms / 1000.0f);
+        temp_x -= pl_vel * (float)sin(pl_angle) * (ms / 1000.0f);
+        temp_y -= pl_vel * (float)cos(pl_angle) * (ms / 1000.0f);
     }
     if (graphics::getKeyState(keyset.down)) {
-        temp_x += vel * (float)sin(angle) * (ms / 1000.0f);
-        temp_y += vel * (float)cos(angle) * (ms / 1000.0f);
+        temp_x += pl_vel * (float)sin(pl_angle) * (ms / 1000.0f);
+        temp_y += pl_vel * (float)cos(pl_angle) * (ms / 1000.0f);
     }
     if (graphics::getKeyState(keyset.left)) {
-        temp_x -= vel * (float)sin(angle + PI / 2) * (ms / 1000.0f);
-        temp_y -= vel * (float)cos(angle + PI / 2) * (ms / 1000.0f);
+        temp_x -= pl_vel * (float)sin((double)pl_angle + PI / 2) * (ms / 1000.0f);
+        temp_y -= pl_vel * (float)cos((double)pl_angle + PI / 2) * (ms / 1000.0f);
     }
     if (graphics::getKeyState(keyset.right)) {
-        temp_x += vel * (float)sin(angle + PI / 2) * (ms / 1000.0f);
-        temp_y += vel * (float)cos(angle + PI / 2) * (ms / 1000.0f);
+        temp_x += pl_vel * (float)sin((double)pl_angle + PI / 2) * (ms / 1000.0f);
+        temp_y += pl_vel * (float)cos((double)pl_angle + PI / 2) * (ms / 1000.0f);
     }
 
     // if the new position is outside of the screen, don't update it
     if (temp_x >= get_canvas_width() * 0.023f && temp_x <= get_canvas_width() * 0.977f)
-        x = temp_x;
+        pl_x = temp_x;
     if (temp_y >= get_canvas_height() * 0.05f && temp_y <= get_canvas_height() - UI::box_height * 2.98f)
-        y = temp_y;
+        pl_y = temp_y;
 
     // decrease or keep at 0
-    remaining = ((0.0f) * (remaining <= 0.0f)) + ((remaining - (ms / 1000.0f)) * (remaining > 0.0f));
+    remaining = (remaining <= 0.0f) ? (0.0f) : (remaining - (ms / 1000.0f));
     // reset if it is 0 and fire is being pressed
-    remaining = ((period) * (remaining == 0.0f && graphics::getKeyState(keyset.fire))) + ((remaining) * !(remaining == 0.0f && graphics::getKeyState(keyset.fire)));
+    remaining = (remaining == 0.0f && graphics::getKeyState(keyset.fire)) ? (period) : (remaining);
 
     return (graphics::getKeyState(keyset.fire)) && (remaining == period);
 }
